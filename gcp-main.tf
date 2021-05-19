21012b746a5618d1bb53ed30e140d3ebb3ced931
Vitor Paiva <paiva.vb@gmail.com>
2021-05-18 22:07:37 -0300

    Terraform GCP Basics

---
+++ gcp-main.tf
@@ -0,0 +1,22 @@
+# Cria uma VM no Google Cloud
+resource "google_compute_instance" "firstvm" {
+  name         = "helloworld"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-c"
+
+  # Define a Imagem da VM
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-bionic-v20210514"
+    }
+  }
+
+  # Habilita rede para a VM com um IP público
+  network_interface {
+    network = "default" # Estamos usando a VPC default que já vem por padrão no projeto.
+
+    access_config {
+    }
+    // A presença do bloco access_config, mesmo sem argumentos, garante que a instância estará acessível pela internet.
+  }
+}
---
+++ providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.68.0"
+    }
+  }
+}
+
+# Configura o Provider Google Cloud com o Projeto
+provider "google" {
+
+  credentials = file("/home/vagrant/lab-devops-cloud-vitor-paiva-83882c5e6c62.json")
+
+  project = "lab-devops-cloud-vitor-paiva"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
